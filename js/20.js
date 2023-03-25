e6241dd978ccbff4fb23cc24e766ef378bdd8be3
DevJuanDuarte <juan.devcontact@gmail.com>
2023-03-25 17:49:17 -0500

    DOM

---
+++ js/20.js
@@ -0,0 +1,11 @@
+//Scope:
+
+const numero = 200
+
+function mismoN() {
+    const numero = 600
+    console.log(numero)
+}
+mismoN()
+
+console.log(numero)
---
+++ js/22.js
@@ -0,0 +1,13 @@
+//DOM:
+
+//querySelector
+const heading = document.querySelector('.heading')
+// console.log(heading);
+// console.log(heading.textContent);
+// console.log(heading.classList);
+// console.log(heading.id);
+// console.log(heading.tagName);
+
+//querySelectorAll
+const enlaces = document.querySelectorAll('.navegacion a')
+console.log(enlaces);
